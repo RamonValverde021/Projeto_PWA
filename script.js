f2280e6788e0db7bc4de00bee2026a7a8fe8169d
Ramon_Valverde <65861376+RamonValverde021@users.noreply.github.com>
2025-10-08 20:48:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const botao = document.getElementById("botao");
     botao.addEventListener("click", () => {
     alert("Você clicou no botão 🚀");
 });
+
+if (window.matchMedia('(display-mode: fullscreen)').matches ||
+    window.matchMedia('(display-mode: standalone)').matches) {
+  document.documentElement.style.backgroundColor = '#000000';
+  document.body.style.backgroundColor = '#000000';
+}
