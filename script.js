5f85f95f5eb603aefabfef0e00ea7ad25c640c09
Ramon_Valverde <65861376+RamonValverde021@users.noreply.github.com>
2025-10-09 09:50:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,3 @@ const botao = document.getElementById("botao");
     botao.addEventListener("click", () => {
     alert("Você clicou no botão 🚀");
 });
-
-const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-if (darkMode) {
-  document.documentElement.classList.add('modo-escuro');
-} else {
-  document.documentElement.classList.add('modo-escuro');
-}
