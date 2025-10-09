ac1eeea7a3a1758e9c9f39ca689299d2c32c2fcd
Ramon_Valverde <65861376+RamonValverde021@users.noreply.github.com>
2025-10-08 21:07:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,8 @@ const botao = document.getElementById("botao");
     alert("Você clicou no botão 🚀");
 });
 
-// Força o tema escuro no Chrome e Android
-document.addEventListener("DOMContentLoaded", () => {
-  let metaColorScheme = document.querySelector('meta[name="color-scheme"]');
-  if (!metaColorScheme) {
-    metaColorScheme = document.createElement("meta");
-    metaColorScheme.name = "color-scheme";
-    document.head.appendChild(metaColorScheme);
-  }
-  metaColorScheme.content = "dark light"; // 'dark' tem prioridade
-
+setTimeout(() => {
   let metaTheme = document.querySelector('meta[name="theme-color"]');
-  if (!metaTheme) {
-    metaTheme = document.createElement("meta");
-    metaTheme.name = "theme-color";
-    document.head.appendChild(metaTheme);
-  }
-  metaTheme.content = "#000000";
-});
+  if (metaTheme) metaTheme.content = "#000000";
+}, 1000);
+
